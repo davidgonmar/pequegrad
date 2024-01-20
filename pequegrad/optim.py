fd9cf905947d52e9ca2459faeb51b7dde211b929
David González Martínez <me@davidgonmar.me>
2024-01-21 00:39:48 +0100

    feat: add weight decay to sgd

--- pequegrad/optim.py
+++ pequegrad/optim.py
@@ -3,9 +3,15 @@ from typing import List
 
 
 class SGD:
-    def __init__(self, parameters: List[Tensor], lr: float = 0.1):
+    def __init__(
+        self,
+        parameters: List[Tensor],
+        lr: float = 0.1,
+        weight_decay: float = 0.0,
+    ):
         self.params = parameters
         self.lr = lr
+        self.weight_decay = weight_decay
 
     def zero(self):
         for p in self.params:
@@ -13,6 +19,6 @@ class SGD:
 
     def step(self, zero: bool = True):
         for p in self.params:
-            p.data -= p.grad.data * self.lr
+            p.data -= (p.grad.data + self.weight_decay * p.data) * self.lr
         if zero:
             self.zero()
