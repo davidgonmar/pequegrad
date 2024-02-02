ecb131d18af569640fe637e1862963aa2f077d76
David González Martínez <me@davidgonmar.me>
2024-02-02 20:35:58 +0100

    fix: make cuda import work on Windows

--- pequegrad/cuda.py
+++ pequegrad/cuda.py
@@ -4,3 +4,19 @@ import sys
 build_path = os.path.join(os.path.dirname(__file__), "..", "build")
 if os.path.exists(build_path):
     sys.path.append(build_path)
+
+# todo - make it output somewhere else and make it general
+windows_vs_build_path = os.path.join(
+    os.path.dirname(__file__), "..", "out", "build", "x64-Debug"
+)
+
+if os.path.exists(windows_vs_build_path):
+    sys.path.append(windows_vs_build_path)
+
+
+# print what python can find
+
+print(sys.path)
+
+
+import pequegrad_cu  # noqa: F401
