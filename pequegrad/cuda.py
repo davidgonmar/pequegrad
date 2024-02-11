6e59ace1d0e2ae6347c516f34af050ea4af66735
David González Martínez <me@davidgonmar.me>
2024-02-11 21:47:43 +0100

    refactor: only accept bin builds in /build

--- pequegrad/cuda.py
+++ pequegrad/cuda.py
@@ -11,26 +11,17 @@ class DummyCudaArray:
         return method
 
 
-build_path = os.path.join(os.path.dirname(__file__), "..", "build")
-if os.path.exists(build_path):
-    sys.path.append(build_path)
-
-# todo - make it output somewhere else and make it general
-windows_vs_build_path = os.path.join(
-    os.path.dirname(__file__), "..", "out", "build", "x64-Debug"
-)
-
-if os.path.exists(windows_vs_build_path):
-    sys.path.append(windows_vs_build_path)
-
-
 SHOULD_USE_CUDA = (
     os.environ.get("PEQUEGRAD_USE_CUDA", "1") == "1"
 )  # by default, use cuda
 
 try:
     if SHOULD_USE_CUDA:
-        from pequegrad_cu import Array as CudaArray  # noqa: F401, E402
+        build_path = os.path.join(os.path.dirname(__file__), "..", "build")
+        if os.path.exists(build_path):
+            sys.path.append(build_path)
+
+        from pequegrad_cu import Array as CudaArray
 
         CUDA_AVAILABLE = True
     else:
