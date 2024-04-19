f9f1536e0812f069903186aca0b8844c7176d3d6
David González Martínez <me@davidgonmar.me>
2024-04-19 16:44:43 +0200

    feat(cpp engine): add as_contiguous on cpu

--- csrc/cpu/view_helpers.cpp
+++ csrc/cpu/view_helpers.cpp
@@ -1,10 +1,14 @@
 #include "view_helpers.hpp"
+#include "./copy_helpers.hpp"
 
 namespace pg {
 namespace cpu {
 namespace view {
 View as_contiguous(const View &view) {
-  return view; // TODO: Implement this
+  View new_view = View(view.shape(), view.dtype(), device::CPU);
+  copy::dispatch_copy(view.shape(), view.strides(), new_view.strides(),
+                      view.get_base_ptr(), new_view.get_base_ptr(), view.dtype());
+  return new_view;
 }
 } // namespace view
 } // namespace cpu
